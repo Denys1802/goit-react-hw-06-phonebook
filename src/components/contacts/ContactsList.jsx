0a2752fadbb14364f545192f9c6b8e01c652866c
DenysZyrianov <78175281+Denys1802@users.noreply.github.com>
2023-04-29 19:18:45 +0300

    redux-toolkit

---
+++ src/components/contacts/ContactsList.jsx
@@ -0,0 +1,36 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { List, Item, ContactsBtn } from './ContactsList.styled';
+import { getContacts, removeContacts } from 'redux/addContactsSlice';
+import { getFilter } from 'redux/filterSlice';
+
+const Contacts = () => {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
+  console.log(contacts);
+
+  const deleteContact = id => dispatch(removeContacts(id));
+
+  const filterContacts = contacts.filter(contact =>
+    contact.name?.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return filterContacts.map(contact => (
+    <List key={contact.id}>
+      <Item>
+        <p>
+          {contact.name}: {contact.number}
+        </p>
+        <ContactsBtn
+          type="button"
+          onClick={() => deleteContact(contact.id)}
+          id={contact.id}
+        >
+          Delete
+        </ContactsBtn>
+      </Item>
+    </List>
+  ));
+};
+
+export default Contacts;
